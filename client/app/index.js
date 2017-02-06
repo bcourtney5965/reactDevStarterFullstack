263698f878534372b1c09182e9155d1a14db4227
Brandon Courtney <brandon.h.courtney@gmail.com>
2017-02-05 16:51:40 -0800

    Deleted comments, reword package.json description

--- client/app/index.js
+++ client/app/index.js
@@ -5,25 +5,4 @@ import { render } from 'react-dom';
 import { DefaultRoute, Link,  Route, RouteHandler } from 'react-router';
 import View1 from './views/View1';
 
-// class View1 extends Component {
-//   constructor() {
-//     super()
-//   }
-//   ajaxTest() {
-//     console.log('hello');
-//     axios.post('/api/test', {"from": "client"})
-//       .then(function (response) {
-//         console.log(response.data);
-//       })
-//       .catch(function (error) {
-//         console.log(error);
-//       });
-//   }
-//   render() {
-//     return (
-//       <div onClick={this.ajaxTest}> Hello World! </div>
-//     );
-//   }
-// }
-
 render(<View1 />, document.getElementById('app'));
