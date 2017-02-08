a38dd1e77cf19f6466277c2382ec7242597a2dfa
Brandon Courtney <brandon.h.courtney@gmail.com>
2017-02-08 14:22:27 -0800

    Built out backend, incl. middleware, routes, controllers

--- server/index.js
+++ server/index.js
@@ -1,21 +1,6 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var bodyParser = require('body-parser');
-
-app.use(express.static(path.join(__dirname, '../client')));
-
+var app = require('./server.js');
 var port = process.env.PORT || 3000;
 
-// middleware
-app.use(bodyParser.json());
-
-app.post('/api/test', function(req, res) {
-  console.log('inside ajax req');
-  console.log(`req.body.from = ${req.body.from}`);
-  res.status(200).json({"from":"server /api/test"});
-})
-
 app.listen(port, () => {
-  console.log(`AJAX with React listening on port# ${port}`);
+  console.log(`RESTful API listening on port: ${port}`);
 })
