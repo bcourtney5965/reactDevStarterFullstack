78355461965df0bba077311947de33f7b73fde53
Brandon Courtney <brandon.h.courtney@gmail.com>
2017-02-15 16:33:14 -0800

    Edit some test code

--- server/server.js
+++ server/server.js
@@ -7,12 +7,12 @@ app.use(express.static(path.join(__dirname, '../client')));
 
 // Test Routes
 app.get('/test',(req, res) => {
-  console.log('inside get reqest');
-  res.status(200).json({"AJAX":"get response from Server"});
+  console.log('inside "GET" reqest');
+  res.status(200).json({"AJAX":"'GET' response from Server"});
 })
 app.post('/test',(req, res) => {
-  console.log('inside post reqest');
-  res.status(200).json({"AJAX":"post response from Server"});
+  console.log('inside "POST" reqest');
+  res.status(200).json({"AJAX":"'POST' response from Server"});
 })
 
 // Mount middleware &
