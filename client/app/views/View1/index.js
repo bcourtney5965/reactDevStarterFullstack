569442ade8dfe4e3eefbc93c682a173b851a472f
Brandon Courtney <brandon.h.courtney@gmail.com>
2017-03-09 18:04:40 -0800

    Add basic table

--- client/app/views/View1/index.js
+++ client/app/views/View1/index.js
@@ -10,7 +10,25 @@ class View1 extends Component {
   }
   render() {
     return (
-      <div onClick={this.testLogger}>View1</div>
+      <table onClick={this.testLogger} id="projects-summary">
+      {/*style={width:100%}*/}
+        <thead>
+          <tr>
+            <th>Name</th> 
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Bob</td> 
+            <td>50</td>
+          </tr>
+          <tr>
+            <td>John</td> 
+            <td>94</td>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 }
